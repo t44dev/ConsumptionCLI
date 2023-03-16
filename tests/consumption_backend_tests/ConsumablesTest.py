dc0f8c093219c40ba0308ea6e31eced5e6758294
track-44 <trackfortyfour@proton.me>
2023-03-17 00:30:51 +1100

    Begun implementation of author tests

--- tests/consumption_backend_tests/ConsumablesTest.py
+++ tests/consumption_backend_tests/ConsumablesTest.py
@@ -1,1 +1,6 @@
 import consumption.consumption_backend.Consumables as cons
+from consumption.consumption_backend.Database import SQLiteDatabaseHandler
+import sqlite3
+
+SQLiteDatabaseHandler.DB_CONNECTION = sqlite3.connect("testdb.db")
+
--- tests/consumption_backend_tests/CreatorsTest.py
+++ tests/consumption_backend_tests/CreatorsTest.py
@@ -1,1 +1,26 @@
 import consumption.consumption_backend.Creators as crea
+from consumption.consumption_backend.Database import SQLiteDatabaseHandler
+import sqlite3
+import unittest
+
+SQLiteDatabaseHandler.DB_CONNECTION = sqlite3.connect("testdb.db")
+
+class TestAuthor(unittest.TestCase):
+
+    def test_save(self):
+        author = crea.Author(None, "John", "Doe", "SavedAuthor")
+        id = author.save()
+        print(crea.Author.get(id))
+    
+    def test_get(self):
+        author = crea.Author(None, "Mary", "Jane", "GetAuthor")
+        id = author.save()
+        print(crea.Author.get(id))
+
+    def test_find(self):
+        author = crea.Author(None, "Max", "Imum", "FoundAuthor")
+        author.save()
+        print(crea.Author.find(author_first_name="Max", author_pseudonym="FoundAuthor"))
+
+if __name__ == '__main__':
+    unittest.main()
