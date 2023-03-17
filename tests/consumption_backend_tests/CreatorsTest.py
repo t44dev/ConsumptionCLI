8e92d4d893499fc7134be3379e1cdb289f915583
track-44 <trackfortyfour@proton.me>
2023-03-17 22:55:36 +1100

    Added tests for Author Database Access

--- tests/consumption_backend_tests/CreatorsTest.py
+++ tests/consumption_backend_tests/CreatorsTest.py
@@ -7,20 +7,45 @@ SQLiteDatabaseHandler.DB_CONNECTION = sqlite3.connect("testdb.db")
 
 class TestAuthor(unittest.TestCase):
 
+    def drop_auth_table(self):
+        SQLiteDatabaseHandler.get_db().cursor().execute("DROP TABLE authors")
+
     def test_save(self):
+        self.drop_auth_table()
         author = crea.Author(None, "John", "Doe", "SavedAuthor")
         id = author.save()
-        print(crea.Author.get(id))
+        author.id = id
+        self.assertIsNotNone(id)
+        get_author = crea.Author.get(id)
+        self.assertEqual(author, get_author)
     
     def test_get(self):
+        self.drop_auth_table()
         author = crea.Author(None, "Mary", "Jane", "GetAuthor")
         id = author.save()
-        print(crea.Author.get(id))
+        author.id = id
+        get_author = crea.Author.get(id)
+        self.assertEqual(author, get_author)
 
     def test_find(self):
-        author = crea.Author(None, "Max", "Imum", "FoundAuthor")
+        self.drop_auth_table()
+        # Found Authors
+        fauthor1 = crea.Author(None, "Max", "Imum", "FoundAuthor")
+        fauthor1.id = fauthor1.save()
+        fauthor2 = crea.Author(None, "Max", "LastName", "FoundAuthor")
+        fauthor2.id = fauthor2.save()
+        fauthor3 = crea.Author(None, "Max", "Imum", "FoundAuthor")
+        fauthor3.id = fauthor3.save()
+        # Unfound Authors
+        author = crea.Author(None, "Max", "Imum", "UnfoundAuthor")
         author.save()
-        print(crea.Author.find(author_first_name="Max", author_pseudonym="FoundAuthor"))
+        author = crea.Author(None, "Min", "Imum", "TestAuthor")
+        author.save()
+        find_authors = crea.Author.find(author_first_name="Max", author_pseudonym="FoundAuthor")
+        f_authors = [fauthor1, fauthor2, fauthor3]
+        self.assertTrue(len(find_authors) == len(f_authors))
+        for author in f_authors:
+            self.assertIn(author, find_authors)
 
 if __name__ == '__main__':
     unittest.main()
