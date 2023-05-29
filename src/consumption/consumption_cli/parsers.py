32b8aaf1e384af1ca970c59fe6c887a5d839c3c4
track44 <trackfortyfour@proton.me>
2023-05-29 15:39:02 +1000

    Added novel parser lines in parser.py

--- src/consumption/consumption_cli/parsers.py
+++ src/consumption/consumption_cli/parsers.py
@@ -22,6 +22,17 @@ main_parser.add_argument("-l", "--list", action="store_true", dest="list")
 ## Novel
 novel_parser = sub_parsers.add_parser("novel")
 novel_parser.set_defaults(_func = novel)
+novel_parser.add_argument("-i", "--id", type=int, dest="id", action=SubdictAction, default=argparse.SUPPRESS)
+novel_parser.add_argument("-n", "--name", dest="name", action=SubdictAction, default=argparse.SUPPRESS)
+novel_parser.add_argument("-v", "--volume", type=int, dest="major_parts", action=SubdictAction, default=argparse.SUPPRESS)
+novel_parser.add_argument("-c", "--chapter", type=int, dest="minor_parts", action=SubdictAction, default=argparse.SUPPRESS)
+novel_parser.add_argument("--rate", "--rating", type=float, dest="rating", action=SubdictAction, default=argparse.SUPPRESS)
+novel_parser.add_argument("--sd", "--startdate", dest="start_date", action=SubdictAction, default=argparse.SUPPRESS)
+novel_parser.add_argument("--ed", "--enddate", dest="end_date", action=SubdictAction, default=argparse.SUPPRESS)
+novel_parser.add_argument("-a", "--authorid", dest="author_id", action=SubdictAction, default=argparse.SUPPRESS)
+# Commands
+novel_parser.add_argument("-r", "--read", dest="read", action="store_true")
+novel_parser.add_argument("-f", "--finish", dest="finish", action="store_true")
 
 ### Creators
 ## Author
@@ -31,5 +42,6 @@ author_parser.add_argument("-i", "--id", type=int, dest="id", action=SubdictActi
 author_parser.add_argument("--fn", "--firstname", dest="first_name", action=SubdictAction, default=argparse.SUPPRESS)
 author_parser.add_argument("--ln", "--lastname", dest="last_name", action=SubdictAction, default=argparse.SUPPRESS)
 author_parser.add_argument("--ps", "--pseudonym", dest="pseudonym", action=SubdictAction, default=argparse.SUPPRESS)
+# Commands
 author_parser.add_argument("--order", choices=["id", "first_name", "last_name", "pseudonym"], default="id")
 author_parser.add_argument("--reverse", action="store_true")
